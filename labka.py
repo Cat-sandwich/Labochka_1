441047384fe5bcd1ca20632e6a4385eab070ce29
Cat-sandwich <beni@ya.ru>
2022-09-25 15:23:38 +0400

    the path to the page with reviews
    now a link to the page with reviews is saved to the file

--- labka.py
+++ labka.py
@@ -6,17 +6,22 @@ url_film_list = []
 
 url='https://www.kinopoisk.ru/lists/movies/top250/'
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
+result = response.text
+soup = BeautifulSoup(result, 'lxml')
 
 
-films = soup.find_all( 'a', class_='base-movie-main-info_link__YwtP1') #находим все фильмы на странице base-movie-main-info_link__YwtP1  styles_main__Y8zDm styles_mainWithNotCollapsedBeforeSlot__x4cWo
-print(films)
+films = soup.find_all('a',  class_='base-movie-main-info_link__YwtP1') #находим все фильмы на странице
+
 print(3)
 
 for film in films:
     url_film = film.get('href')
     url_film_list.append(url_film)
     
-with open('url_film_list.txt', 'a') as file:
+with open('url_film_list.txt', 'w') as file: # сохранение ссылок в файл
     for line in url_film_list:
-        file.write(f'{line}\n')
+        file.write( 'https://www.kinopoisk.ru' + f'{line}' + 'reviews/\n')
+
+def function ():
+   with open('url_film_list.txt', 'w') as file:
+    file.readline() 
